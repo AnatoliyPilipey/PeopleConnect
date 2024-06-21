650638fe421bebec7489467bbc9406a9f920f313
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-06-21 19:14:17 +0300

    Add in views.py ProfileViewSet & SubscribeViewSet

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import viewsets, mixins, status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import F, Count
+from chat.permissions import IsAdminOrIfAuthenticatedReadOnly
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from chat.models import (
+    Profile,
+    Subscribe,
+)
+from chat.serializers import (
+    ProfileSerializer,
+    SubscribeSerializer,
+)
 
-# Create your views here.
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """Profile"""
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class SubscribeViewSet(viewsets.ModelViewSet):
+    """Subscribe"""
+    queryset = Subscribe.objects.all()
+    serializer_class = SubscribeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
