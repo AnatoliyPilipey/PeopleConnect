4325b3f6ec2e766fdf3293fcbb54da68e02a7e3b
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-06-21 19:19:52 +0300

    Add urls for profile & subscribe

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
+from chat.views import (
+    ProfileViewSet,
+    SubscribeViewSet,
+)
 
 
 router = routers.DefaultRouter()
+router.register("profile", ProfileViewSet)
+router.register("subscribe", SubscribeViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
