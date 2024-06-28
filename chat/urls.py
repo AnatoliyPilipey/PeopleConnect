328e68f04ec7f7f7805b9bcb711e3773ca27cdc3
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-06-28 08:37:52 +0300

    Add url for message

--- chat/urls.py
+++ chat/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 from chat.views import (
     ProfileViewSet,
+    MessageViewSet,
 )
 
 
 router = routers.DefaultRouter()
 router.register("profile", ProfileViewSet)
-
+router.register("message", MessageViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
